d8e469d86cb22a46bbe1ab95e976138bdc958033
Andrei Solntsev <andrei.solntsev@gmail.com>
2013-07-13 00:03:42 +0300

    $$.findBy waits until element matches the condition

--- github/test/org/selenide/examples/github/RepositoryPage.java
+++ github/test/org/selenide/examples/github/RepositoryPage.java
@@ -1,17 +1,19 @@
 package org.selenide.examples.github;
 
 import com.codeborne.selenide.SelenideElement;
-import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class RepositoryPage {
   public void openSource(String path) {
-    $(By.xpath("//td[contains(@class,'content')]//a[text()='" + path + "']")).click();
-//    $$(".tree-browser .content").findBy(text(path)).followLink();
+//    $(By.xpath("//td[contains(@class,'content')]//a[text()='" + path + "']")).click();
+
+    $$("td.content").findBy(text(path)).find("a").followLink();
   }
 
   public SelenideElement getSourceLines() {
-    return $(".lines");
+    return $(".file-code");
   }
 }
